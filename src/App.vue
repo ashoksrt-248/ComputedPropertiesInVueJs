6dcf15cca0e75d770090b2fde6b914bc6d8cb981
Ashok Amara <ashok.amara@cesneosilica.com>
2024-04-03 11:18:24 +0530

    computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
 <template>
   <div id="app">
     <h1>Computed Properties in VueJS</h1>
+    <h2>Full Name is  {{ firstName }} {{ lastName }}</h2>
+    <h2>Computed Full Name is  {{ fullName }}</h2>
+    <h2>Total Price is  {{ total }}</h2>
     
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      firstName:'Ashok',
+      lastName:'Amara',
+      items:[
+        {
+          id:1,
+          title:'Tv',
+          price:100
+        },
+        {
+          id:2,
+          title:'Phone',
+          price:200
+        },
+        {
+          id:3,
+          title:'Laptop',
+          price:300
+        },
+      ],
+    }
+  },
+  methods:{
+
+  },
+  computed:{
+    fullName(){
+      return `${this.firstName} ${this.lastName}`
+    },
+    total(){
+      return this.items.reduce((total, curr) => (total = total+ curr.price), 0)
+    }
+  }
 }
 </script>
 
